c000b026a87ebb6f1c77ae2d3a5092cfe658d1be
maryem <zanned.maryem@gmail.com>
2026-04-10 17:44:42 +0100

    add 12-binary_tree_leaves.c file with update

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -17,3 +17,18 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	}
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
+/**
+ * binary_tree_is_leaf - function that checks if a node is a leaf
+ *@node: node to check.
+ *Return: 1 if node is a leaf, otherwise 0.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->left != NULL || node->right != NULL)
+	{
+		return (0);
+	}
+	binary_tree_is_leaf(node->left);
+	binary_tree_is_leaf(node->right);
+	return (1);
+}

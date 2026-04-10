386241d00a5b39fc9f28b776060a95459c070539
maryem <zanned.maryem@gmail.com>
2026-04-10 12:54:05 +0100

    add 4-binary_tree_is_leaf.c file and update with betty coding style

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,19 +1,17 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- * binary_tree_is_leaf - function that inserts a node
- * as the left-child of another node.
+ * binary_tree_is_leaf - function that checks if a node is a leaf
  *@node: node to check.
- *Return: pointer to the created node,
- *or NULL on failure or if parent is NULL
+ *Return: 1 if node is a leaf, otherwise 0.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL || node->left != NULL || node->right != NULL)
-    {
-        return(0);
-    }
-    binary_tree_is_leaf(node->left);
-    binary_tree_is_leaf(node->right);
-    return(1);
+	if (node == NULL || node->left != NULL || node->right != NULL)
+	{
+		return (0);
+	}
+	binary_tree_is_leaf(node->left);
+	binary_tree_is_leaf(node->right);
+	return (1);
 }

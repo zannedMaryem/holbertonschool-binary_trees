4d59a8402f67bd5276d099be1527bd1983cf54ed
maryem <zanned.maryem@gmail.com>
2026-04-10 22:33:05 +0100

    add 15-binary_tree_is_full.c file

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,17 +7,18 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        return (1);
-    }
-    if (tree->left != NULL && tree->right != NULL)
-    {
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    }
-    return (0);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left) &&
+		binary_tree_is_full(tree->right));
+	}
+	return (0);
 }

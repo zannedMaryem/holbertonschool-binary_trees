a4f14c2b7944f9d00e2c169b5467f5b9490ee1b4
maryem <zanned.maryem@gmail.com>
2026-04-11 11:52:22 +0100

    adding modifications to 16-binary_tree_is_perfect.c file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,7 +7,8 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
+
 	if (tree == NULL)
 	{
 		return (0);
@@ -26,7 +27,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	return (binary_tree_is_perfect(tree->left)
+	&& binary_tree_is_perfect(tree->right));
 }
 /**
  * binary_tree_height - function that measures the height of a binary tree
